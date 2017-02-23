90cb1d5e54e6beb8ab6ca1dcc1bbd8f38051f73d
Becky Chan <beckychan@Beckys-MacBook-Air.local>
2017-02-22 18:55:39 -0800

    update VC with text field and buttons

--- lab4/ViewController.swift
+++ lab4/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func signUp(_ sender: Any) {
+    }
+    
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
